456d6de6319e3628cd73f2a9fc55df508703e78e
Majd Sheikh <majd_sheikh@hotmail.com>
2022-12-07 14:42:44 +0200

    test

---
+++ firstreact/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+      <h1>Hello Dojo!</h1>
+      <h2>Things i need to do:</h2>
+        <li>Learn React</li>
+        <li>Climb Mt Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+<p>gsge</p>
+      </>
+    </div>
+  );
+}
+
+export default App;
